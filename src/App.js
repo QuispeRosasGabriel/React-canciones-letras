beb06587b2a11a4dd8da859bdef4533cbdc2a9cc
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-15 17:46:27 -0500

    consultando 2 apis

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import axios from "axios";
+import Cancion from "./components/Cancion";
 
 function App() {
   // definir state
@@ -12,15 +13,32 @@ function App() {
     const consultarApiLetra = async () => {
       const { artista, cancion } = busquedaLetra;
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const resultado = await axios.get(url);
-      guardarLetra(resultado.data.lyrics);
+      const url2 = `https://www.theaudiodb.com/api/v1/json/search.php?s=${artista}`;
+
+      const [letra, informacion] = await Promise.all([
+        axios.get(url),
+        axios.get(url2),
+      ]);
+
+      console.log(letra);
+      console.log(informacion);
+
+      // guardarLetra(resultado.data.lyrics);
     };
     consultarApiLetra();
   }, [busquedaLetra]);
 
   return (
     <Fragment>
-      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />.
+      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">1</div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
